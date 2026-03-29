696c1fb5714e84b239dd0885567a0472bff8ec22
dolotallaschristian2-hub <dolotallaschristian2@gmail.com>
2026-03-29 22:24:41 +0800

    Update Giga.py

--- Giga.py
+++ Giga.py
@@ -1,12 +1,12 @@
 import random
 
-
 from Characters import Character
 
-class Giga (Character):
+
+class Giga(Character):
 
     def __init__(self):
-        super().__init__("Giga", 180, 20, 0, 0, 70, 70, 0)
+        super().__init__("Giga", 180, 20, 0, 0, 50, 70, 0)
 
         self.skills = {
             1: self.basic_attack,
@@ -15,89 +15,70 @@ class Giga (Character):
             4: self.skill_3
         }
 
-def basic_attack(self, enemy):
-    print("Used Basic Attack!")
-    dmg = self.Atk
-    
-    # If skill 1 is activated
-    if self.has_buff("Double Damage"):
-        print(f"{self.Name} has double damage!")
-        dmg *= 2  # apply the effect
-    
-    else:
-        print("Normal damage.")
-    
-    # Apply damage to enemy (optional, if you have HP system)
-    enemy.hp -= dmg
-    print(f"{enemy.name} took {dmg} damage!")
+    def take_damage(self, dmg, enemy):
 
-    return True
+        if self.has_buff("Invincible"):
+            print(f"{self.Name} is invincible! No damage taken!")
+            return
 
-def skill_1 (self, enemy):
+        self.Hp -= dmg
+        print(f"{self.Name} took {dmg} damage!")
 
-    print (" Giga used Skill 1! ")
-    # GIGA SMASH
-    # Everytime GigaSmash skill activates, doubling the next basic attack and reducing the cooldown of the other skill
-        
-def skill_2(self, enemy):
-    print("Giga used Skill 2!")
-    
-    # GIGA TURN
-    # Negates damage for 2 turns
-    self.add_buff("Damage Immunity", 2)
-    
-    print(f"{self.Name} is immune to damage for 2 turns!")
-    
-    return True
 
-def skill_3(self):
-    print("Giga used Skill 3!")
-    
-    # Save original stats only if not already buffed
-    if not hasattr(self, "original_stats"):
-        self.original_stats = {
-            "MaxHp": self.MaxHp,
-            "Hp": self.Hp,
-            "Atk": self.Atk,
-            "Spd": self.Spd,
-            "Accuracy": self.Accuracy
-        }
+    def basic_attack(self, enemy):
+        if self.check_hit(enemy):
+            print("Used Basic Attack!")
+            dmg = self.Atk * 0.5
+            enemy.take_damage(dmg , enemy)
 
-    # Apply buff
-    self.MaxHp *= 2
-    self.Hp *= 2
-    self.Atk *= 2
-    self.Spd *= 2
-    self.Accuracy *= 2
+            # If skill 1 is activated
+            if self.has_buff("Double Damage"):
+                print(f"{self.Name} has double damage!")
+                dmg += dmg  # apply the effect
+                enemy.take_damage(dmg , enemy)
 
-    # Set duration
-    self.skill3_turns = 3
+            else:
+                print("Normal damage.")
+        else:
+             print(f"{enemy.Name} evaded your attack!")
 
-    print(f"{self.Name}'s stats are doubled for 4 turns!")
 
-    return True
+    def skill_1(self, enemy):
+        print(" Giga used Skill 1! ")
+        self.add_buff("Double Damage", 1)
 
-# Skill 3 buffs for calculating stats
 
-def update_buffs(self):
-    if hasattr(self, "skill3_turns") and self.skill3_turns > 0:
-        self.skill3_turns -= 1
+    def skill_2(self, enemy):
+        print("Giga used Skill 2!")
 
-        if self.skill3_turns == 0:
-            # Revert stats
-            self.MaxHp = self.original_stats["MaxHp"]
-            self.Hp = min(self.Hp, self.MaxHp)  # avoid overflow
-            self.Atk = self.original_stats["Atk"]
-            self.Spd = self.original_stats["Spd"]
-            self.Accuracy = self.original_stats["Accuracy"]
+        # GIGA TANK
+        # Negates damage for 2 turns
+        self.add_buff("Invincible", 2)
 
-            del self.original_stats
-            print(f"{self.Name}'s stats returned to normal!")
+        print(f"{self.Name} is immune to damage for 2 turns!")
 
-def skill_cd (self, move):
-    if move == 2:
-        return 2
-    if move == 3:
-        return 2
-    if move == 4:
-        return 9
+        return True
+
+
+    def skill_3(self, enemy):
+        print("Giga used Skill 3!")
+        # NINJA THIEVES
+        self.add_buff("Enhanced", 2)
+
+        print(f"{self.Name}'s stats are doubled for 2 turns!")
+
+        return True
+
+    def end_turn_checks(self):
+        while "Enhanced" in self.buffs:
+            self.Atk *= 2
+            self.Hp *= 2
+
+    def get_skill_cd(self, move):
+        if move == 2:
+            return 1
+        if move == 3:
+            return 3
+        if move == 4:
+            return 3
+        return 0
